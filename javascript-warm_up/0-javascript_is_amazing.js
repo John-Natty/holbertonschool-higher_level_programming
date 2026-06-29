4cc39c8314ee99fec9564da4ed31e2fc60e46e79
John-Natty <jonathanmerle8426@gmail.com>
2026-06-29 14:35:01 +0200

    Add JavaScript files to demonstrate variable declaration and output

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
+// Create a constant variable with the required text
 const myVar = 'JavaScript is amazing';
+
+// Print the value of the variable
 console.log(myVar);
---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Create a constant variable with the required text
+const myVar = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'JavaScript is amazing';
+
+// Print the value of the variable
+console.log(myVar);
+console.log(myVar1);
+console.log(myVar2);
