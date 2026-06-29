1f9af1f5c3dda8f20bb74ee16ff2e70d8cd80e96
John-Natty <jonathanmerle8426@gmail.com>
2026-06-29 15:46:05 +0200

    Add script to concatenate two command line arguments with "is" in between

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Store the first argument passed to the script
+const firstArgument = process.argv[2];
+
+// Store the second argument passed to the script
+const secondArgument = process.argv[3];
+
+// Print both arguments with "is" between them
+console.log(firstArgument + ' is ' + secondArgument);
