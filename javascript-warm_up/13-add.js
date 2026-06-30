cab9e6efe270c090603b7b41f8e8caab09b85a82
John-Natty <jonathanmerle8426@gmail.com>
2026-06-30 14:30:09 +0200

    Add script to implement addition of two numbers

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Return the addition of two numbers
+function add (a, b) {
+  return a + b;
+}
+
+// Export the add function so it can be used in another file
+exports.add = add;
