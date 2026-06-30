45e58c3ee70810609b32e814c56d6e45629b0793
John-Natty <jonathanmerle8426@gmail.com>
2026-06-30 14:22:54 +0200

    Add script to modify the value property of an object

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+// Change the value property of the object
+myObject.value = 89;
+
+console.log(myObject);
