3fe916642bbbd3bc3b1b5218562f5c09c67b3071
John-Natty <jonathanmerle8426@gmail.com>
2026-06-30 13:32:58 +0200

    Add script to print predefined programming language sentences

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Store the three required sentences in an array
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Loop through the array and print each sentence
+for (const language of languages) {
+  console.log(language);
+}
