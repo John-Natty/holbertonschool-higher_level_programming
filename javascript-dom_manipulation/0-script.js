39a2a5a374ccf0ba226896f10ecff277bc0b2ec4
John-Natty <jonathanmerle8426@gmail.com>
2026-07-01 09:15:27 +0200

    Add script for DOM manipulation and create README with objectives and requirements

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Select the header element from the HTML document.
+const header = document.querySelector('header');
+
+// Change the text color of the header to red.
+header.style.color = '#FF0000';
